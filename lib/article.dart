e9a51fb0480cb43666fd20ee0f5b4f2b0b2ccaef
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-08 11:01:48 +0800

    Update article.dart
    Menambahkan named constructor

--- lib/article.dart
+++ lib/article.dart
@@ -16,4 +16,14 @@ class Article {
     required this.publishedAt,
     required this.content,
   });
+
+  Article.fromJson(Map<String, dynamic> article) {
+    author = article['author'];
+    title = article['title'];
+    description = article['description'];
+    url = article['url'];
+    urlToImage = article['urlToImage'];
+    publishedAt = article['publishedAt'];
+    content = article['content'];
+  }
 }
