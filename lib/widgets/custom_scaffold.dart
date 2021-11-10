a026629dc3f296a80f347cc89da9140cf212890a
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-10 09:31:52 +0800

    Create custom_scaffold.dart

---
+++ lib/widgets/custom_scaffold.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CustomScaffold extends StatelessWidget {
+  final Widget body;
+
+  const CustomScaffold({Key? key, required this.body}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            body,
+          ],
+        ),
+      ),
+    );
+  }
+}
