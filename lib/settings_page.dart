98e80417a997f1246b3bfd6d6205ec1e5b4a3e90
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-11 12:03:02 +0800

    Create settings_page.dart

---
+++ lib/settings_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_news_app/widgets/platform_widget.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({Key? key}) : super(key: key);
+
+  Widget _buildList(BuildContext context) {
+    return ListView(
+      children: [
+        Material(
+          child: ListTile(
+            title: Text('Dark Theme'),
+            trailing: Switch.adaptive(
+              value: false,
+              onChanged: (value) {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text('Coming Soon!'),
+                      content: Text('This feature will be coming soon!'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text('Ok'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildAndroid(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      body: _buildList(context),
+    );
+  }
+
+  Widget _buildIos(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Settings'),
+      ),
+      child: _buildList(context),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformWidget(
+      androidBuilder: _buildAndroid,
+      iosBuilder: _buildIos,
+    );
+  }
+}
