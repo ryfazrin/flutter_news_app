ec370b78f4fdbcb76333b75b705ab725e9624a1c
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-11 11:23:13 +0800

    Create article_list_page.dart

---
+++ lib/article_list_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_news_app/article.dart';
+import 'package:flutter_news_app/detail_page.dart';
+import 'package:flutter_news_app/widgets/platform_widget.dart';
+
+class ArticleListPage extends StatelessWidget {
+  const ArticleListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformWidget(
+      androidBuilder: _buildAndroid,
+      iosBuilder: _buildIos,
+    );
+  }
+
+  Widget _buildAndroid(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('News App')),
+      body: _buildList(context),
+    );
+  }
+
+  Widget _buildIos(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('News App'),
+      ),
+      child: _buildList(context),
+    );
+  }
+
+  Widget _buildList(BuildContext context) {
+    return FutureBuilder<String>(
+      future: DefaultAssetBundle.of(context).loadString('assets/articles.json'),
+      builder: (context, snapshot) {
+        final List<Article> articles = parseArticles(snapshot.data);
+        return ListView.builder(
+          itemCount: articles.length,
+          itemBuilder: (BuildContext context, int index) {
+            return _buildArticleItem(context, articles[index]);
+          },
+        );
+      },
+    );
+  }
+
+  Widget _buildArticleItem(BuildContext context, Article article) {
+    return Material(
+      child: ListTile(
+        onTap: () {
+          Navigator.pushNamed(context, ArticleDetailPage.routeName,
+              arguments: article);
+        },
+        contentPadding:
+            const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        leading: Image.network(
+          article.urlToImage,
+          width: 100,
+        ),
+        title: Text(article.title),
+        subtitle: Text(article.author),
+      ),
+    );
+  }
+}
