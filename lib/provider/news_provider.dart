e8b36703fd9c6b12ba0e13fe1fbe761c5e4bad6c
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-14 11:12:24 +0800

    Update news_provider.dart
    Tambahkan Class NewsProvider yang bertugas melakukan proses pengambilan data dari internet.

---
+++ lib/provider/news_provider.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_news_app/data/api/api_service.dart';
+import 'package:flutter_news_app/data/model/article.dart';
+
+enum ResultState { Loading, NoData, HasData, Error }
+
+class NewsProvider extends ChangeNotifier {
+  final ApiService apiService;
+
+  NewsProvider(this.apiService) {
+    _fetchAllArticle();
+  }
+
+  late ArticlesResult _articlesResult;
+  late ResultState _state;
+  String _message = '';
+
+  String get message => _message;
+
+  ArticlesResult get result => _articlesResult;
+
+  Future<dynamic> _fetchAllArticle() async {
+    try {
+      _state = ResultState.Loading;
+      notifyListeners();
+      final article = await apiService.topHeadlines();
+      if (article.articles.isEmpty) {
+        _state = ResultState.NoData;
+        notifyListeners();
+        return _message = 'Empty Data';
+      } else {
+        _state = ResultState.HasData;
+        notifyListeners();
+        return _articlesResult = article;
+      }
+    } catch (e) {
+      _state = ResultState.Error;
+      notifyListeners();
+      return _message = 'Error --> $e';
+    }
+  }
+}
