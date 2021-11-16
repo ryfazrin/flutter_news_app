1319dd52a91655370f3cf0eabd128191ef516234
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-16 09:42:16 +0800

    Update api_service.dart

--- lib/data/api/api_service.dart
+++ lib/data/api/api_service.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
   static final String _baseUrl = 'https://newsapi.org/v2/';
-  static final String _apiKey = 'MY_API_KEY';
+  static final String _apiKey = '86241637f163493ebc458d087b7ee7ec';
   static final String _category = 'business';
   static final String _country = 'id';
 
