21ed3a5dcdd839f77f8e1b1f1de41187b6ed38dc
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-16 09:36:54 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,33 @@
+import 'dart:io';
+
+import 'package:android_alarm_manager/android_alarm_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_news_app/common/navigation.dart';
 import 'package:flutter_news_app/common/styles.dart';
 import 'package:flutter_news_app/data/model/article.dart';
 import 'package:flutter_news_app/ui/article_detail_page.dart';
 import 'package:flutter_news_app/ui/article_web_view.dart';
 import 'package:flutter_news_app/ui/home_page.dart';
+import 'package:flutter_news_app/utils/background_service.dart';
+import 'package:flutter_news_app/utils/notification_helper.dart';
 
-void main() {
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final NotificationHelper _notificationHelper = NotificationHelper();
+  final BackgroundService _service = BackgroundService();
+
+  _service.initializeIsolate();
+
+  if (Platform.isAndroid) {
+    await AndroidAlarmManager.initialize();
+  }
+  await _notificationHelper.initNotifications(flutterLocalNotificationsPlugin);
+
   runApp(const MyApp());
 }
 
@@ -44,6 +66,7 @@ class MyApp extends StatelessWidget {
           unselectedItemColor: Colors.grey,
         ),
       ),
+      navigatorKey: navigatorKey,
       initialRoute: HomePage.routeName,
       routes: {
         HomePage.routeName: (context) => HomePage(),
