269ae9b263061a037dea1a074962702ca5ecf341
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-16 09:09:15 +0800

    Update custom_dialog.dart

--- lib/widgets/custom_dialog.dart
+++ lib/widgets/custom_dialog.dart
@@ -4,7 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_news_app/common/navigation.dart';
 
-CustomDialog(BuildContext context) {
+customDialog(BuildContext context) {
   if (Platform.isIOS) {
     showCupertinoDialog(
         context: context,
