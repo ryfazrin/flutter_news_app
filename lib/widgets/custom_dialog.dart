df72226ef5a761860056c25980ce0ab5ba534fed
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-16 09:02:28 +0800

    Update custom_dialog.dart

---
+++ lib/widgets/custom_dialog.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_news_app/common/navigation.dart';
+
+CustomDialog(BuildContext context) {
+  if (Platform.isIOS) {
+    showCupertinoDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: Text('Coming Soon!'),
+            content: Text('This feature will be coming soon!'),
+            actions: [
+              CupertinoDialogAction(
+                child: Text('Ok'),
+                onPressed: () {
+                  Navigation.back();
+                },
+              ),
+            ],
+          );
+        });
+  } else {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Coming Soon!'),
+            content: Text('This feature will be coming soon!'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigation.back();
+                },
+                child: Text('Ok'),
+              ),
+            ],
+          );
+        });
+  }
+}
