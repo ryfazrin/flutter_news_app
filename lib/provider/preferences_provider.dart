67cfbd0ed4507230faedd99feed6cac7a6f14013
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-17 23:43:04 +0800

    Create preferences_provider.dart

---
+++ lib/provider/preferences_provider.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_news_app/preferences/preferences_helper.dart';
+
+class PreferencesProvider extends ChangeNotifier {
+  PreferencesHelper preferencesHelper;
+
+  PreferencesProvider({required this.preferencesHelper});
+
+  bool _isDarkTheme = false;
+  bool get isDarkTheme => _isDarkTheme;
+
+  bool _isDailyNewsActive = false;
+  bool get isDailyNewsactive => _isDailyNewsActive;
+
+  void _getTheme() async {
+    _isDarkTheme = await preferencesHelper.isDarkTheme;
+    notifyListeners();
+  }
+
+  void _getDailyNewsPreferences() async {
+    _isDailyNewsActive = await preferencesHelper.isDailyNewsActive;
+    notifyListeners();
+  }
+
+  void enableDarkTheme(bool value) {
+    preferencesHelper.setDarkTheme(value);
+    _getTheme();
+  }
+
+  void enableDailyNews(bool value) {
+    preferencesHelper.setDailyNews(value);
+    _getDailyNewsPreferences();
+  }
+}
