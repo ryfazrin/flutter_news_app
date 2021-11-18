84871c913645b337cbe05e09de58ea379e2d10db
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-18 09:40:01 +0800

    Update card_article.dart
    membutuhkan Consumer karena widget perlu mendengarkan perubahan data dari provider. Selain itu, metode isBookmarked mengembalikan nilai Future, artinya kita juga perlu menggunakan widget FutureBuilder

--- lib/widgets/card_article.dart
+++ lib/widgets/card_article.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_news_app/common/navigation.dart';
 import 'package:flutter_news_app/data/model/article.dart';
+import 'package:flutter_news_app/provider/database_provider.dart';
 import 'package:flutter_news_app/ui/article_detail_page.dart';
+import 'package:provider/provider.dart';
 
 class CardArticle extends StatelessWidget {
   final Article article;
@@ -10,21 +12,29 @@ class CardArticle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: ListTile(
-        contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-        leading: Hero(
-          tag: article.urlToImage!,
-          child: Image.network(
-            article.urlToImage!,
-            width: 100,
-          ),
-        ),
-        title: Text(article.title),
-        subtitle: Text(article.author!),
-        onTap: () =>
-            Navigation.intentWithData(ArticleDetailPage.routeName, article),
-      ),
-    );
+    return Consumer<DatabaseProvider>(builder: (context, provider, child) {
+      return FutureBuilder<bool>(
+          future: provider.isBookmarked(article.url),
+          builder: (context, snapshot) {
+            var isBookmarked = snapshot.data ?? false;
+            return Material(
+              child: ListTile(
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                leading: Hero(
+                  tag: article.urlToImage!,
+                  child: Image.network(
+                    article.urlToImage!,
+                    width: 100,
+                  ),
+                ),
+                title: Text(article.title),
+                subtitle: Text(article.author!),
+                onTap: () => Navigation.intentWithData(
+                    ArticleDetailPage.routeName, article),
+              ),
+            );
+          });
+    });
   }
 }
