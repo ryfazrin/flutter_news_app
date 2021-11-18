02725ddae33214565fdbe893903fdde3e4cd360e
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-18 08:38:19 +0800

    Update settings_page.dart

--- lib/ui/settings_page.dart
+++ lib/ui/settings_page.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_news_app/provider/preferences_provider.dart';
 import 'package:flutter_news_app/provider/scheduling_provider.dart';
 import 'package:flutter_news_app/widgets/custom_dialog.dart';
 import 'package:flutter_news_app/widgets/platform_widget.dart';
@@ -14,37 +15,44 @@ class SettingsPage extends StatelessWidget {
   static const String settingsTitle = 'Settings';
 
   Widget _buildList(BuildContext context) {
-    return ListView(
-      children: [
-        Material(
-          child: ListTile(
-            title: Text('Dark Theme'),
-            trailing: Switch.adaptive(
-              value: false,
-              onChanged: (value) => customDialog(context),
-            ),
-          ),
-        ),
-        Material(
-          child: ListTile(
-            title: Text('Scheduling News'),
-            trailing: Consumer<SchedulingProvider>(
-              builder: (context, scheduled, _) {
-                return Switch.adaptive(
-                  value: scheduled.isScheduled,
-                  onChanged: (value) async {
-                    if (Platform.isIOS) {
-                      customDialog(context);
-                    } else {
-                      scheduled.scheduleNews(value);
-                    }
+    return Consumer<PreferencesProvider>(
+      builder: (context, provider, child) {
+        return ListView(
+          children: [
+            Material(
+              child: ListTile(
+                title: Text('Dark Theme'),
+                trailing: Switch.adaptive(
+                  value: provider.isDarkTheme,
+                  onChanged: (value) {
+                    provider.enableDailyNews(value);
                   },
-                );
-              },
+                ),
+              ),
             ),
-          ),
-        ),
-      ],
+            Material(
+              child: ListTile(
+                title: Text('Scheduling News'),
+                trailing: Consumer<SchedulingProvider>(
+                  builder: (context, scheduled, _) {
+                    return Switch.adaptive(
+                      value: provider.isDailyNewsActive,
+                      onChanged: (value) async {
+                        if (Platform.isIOS) {
+                          customDialog(context);
+                        } else {
+                          scheduled.scheduleNews(value);
+                          provider.enableDailyNews(value);
+                        }
+                      },
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 
