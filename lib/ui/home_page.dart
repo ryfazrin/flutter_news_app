2835b46fe980064aba4c527a8b104905742be684
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-11-18 08:38:03 +0800

    Hapus ChangeNotifierProvider

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -2,15 +2,11 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_news_app/data/api/api_service.dart';
-import 'package:flutter_news_app/provider/news_provider.dart';
-import 'package:flutter_news_app/provider/scheduling_provider.dart';
 import 'package:flutter_news_app/ui/article_detail_page.dart';
 import 'package:flutter_news_app/ui/article_list_page.dart';
 import 'package:flutter_news_app/ui/settings_page.dart';
 import 'package:flutter_news_app/utils/notification_helper.dart';
 import 'package:flutter_news_app/widgets/platform_widget.dart';
-import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   static const routeName = '/home_page';
@@ -73,14 +69,9 @@ class _HomePageState extends State<HomePage> {
   ];
 
   final List<Widget> _listWidget = [
-    ChangeNotifierProvider(
-      create: (_) => NewsProvider(apiService: ApiService()),
-      child: ArticleListPage(),
-    ),
-    ChangeNotifierProvider<SchedulingProvider>(
-      create: (_) => SchedulingProvider(),
-      child: SettingsPage(),
-    ),
+    ArticleListPage(),
+    BookmarksPage(),
+    SettingsPage(),
   ];
 
   @override
